d0480a9c15e14d8f026cf3f29fcd4959139c1d4f
Felipe <snt.feeeh@gmail.com>
2024-06-09 22:52:30 -0300

    Comentarios e Pausar o Programa

--- Primeiro Programa/main.c
+++ Primeiro Programa/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+aprendendo Fazer um comentário
+*/
+
 int main()
 {
     printf("Meu Primeiro programa!\n");
+
+    system("pause");
     return 0;
 }
